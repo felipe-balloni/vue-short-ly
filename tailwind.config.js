88e13b85bf165fc1ff3909f65650aa3ffca5bfd6
Felipe Balloni Ferreira <felipe.balloni@hotmail.com>
2023-08-28 17:17:17 -0300

    feat: add package tw-elements

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-    content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+    content: [
+        './index.html',
+        './src/**/*.{vue,js,ts,jsx,tsx}',
+        "./node_modules/tw-elements/dist/js/**/*.js"],
     theme: {
         extend: {
             fontFamily: {
@@ -9,5 +12,9 @@ module.exports = {
             },
         },
     },
-    plugins: [require('@tailwindcss/forms')],
+    darkMode: 'class',
+    plugins: [
+        require('@tailwindcss/forms'),
+        require("tw-elements/dist/plugin.cjs")
+    ]
 }
